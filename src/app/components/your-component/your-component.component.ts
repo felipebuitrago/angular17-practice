044e4d229e87bf0e34f76e7081470518630aa511
Felipe Buitrago <flipe282000@gmail.com>
2023-12-06 01:32:24 -0500

    ng g c component/name & ng g m modules/name

---
+++ src/app/components/your-component/your-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './your-component.component.html',
+  styleUrl: './your-component.component.css'
+})
+export class YourComponentComponent {
+
+}
